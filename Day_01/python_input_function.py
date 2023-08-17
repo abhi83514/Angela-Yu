3e397adb03d5c5e8e369319ac10776330436331e
Shivendra Rajput <118388724+abhi83514@users.noreply.github.com>
2023-08-18 03:16:57 +0530

    Config

--- Day_01/python_input_function.py
+++ Day_01/python_input_function.py
@@ -2,6 +2,9 @@
  
 input("A prompt for the user")
 
-#input() will get user input in console 
-#Then print() will print the word "Hello" and the user input
+
+
+# input() will get user input in console 
+# Then print() will print the word "Hello" and the user input
+
 print("hello" + input("What is your name?\n"))
